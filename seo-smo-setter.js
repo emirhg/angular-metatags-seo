3321de3e45ebf6ca46271bcf8d8cae7017eb7d91
Emir Herrera <emir.herrera@imco.org.mx>
2015-11-08 19:04:17 -0600

    Facebook share

--- seo-smo-setter.js
+++ seo-smo-setter.js
@@ -1,1 +1,47 @@
-//Hello world
+/*global FB:false*/
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name indicesApp.seo
+ * @description para optimizar y social
+ * # seo
+ * Service in the indicesApp.
+ */
+angular.module('SEO-SMO')
+    .service('seo', function seo($rootScope) {
+        // AngularJS will instantiate a singleton by calling "new" on this function
+
+        /** Facebook SDK */
+        window.fbAsyncInit = function() {
+            FB.init({
+                appId: '141448832714787',
+                xfbml: true,
+                version: 'v2.0'
+            });
+        };
+
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {
+                return;
+            }
+            js = d.createElement(s);
+            js.id = id;
+            js.src = '//connect.facebook.net/en_US/sdk.js';
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+
+        $rootScope.fbShare = function(href) {
+
+            if (typeof href === 'undefined') {
+                href = location.href;
+            }
+            FB.ui({
+                method: 'share',
+                display: 'popup',
+                href: href
+            }, function() {});
+        };
+        /** End of Facebook SDK **/
+    });
