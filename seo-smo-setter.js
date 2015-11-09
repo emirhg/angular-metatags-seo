af9b372da46e7d59ad7d0db615890c5b202cd6c9
Emir Herrera <emir.herrera@imco.org.mx>
2015-11-09 01:25:02 -0600

    Metatags

--- seo-smo-setter.js
+++ seo-smo-setter.js
@@ -35,8 +35,66 @@ angular.module('SEO-SMO', [])
         }
 
         this.$get = [];
+    }).service('metatags', function(){
+        var setMetatag = function(name, value){
+            var meta = $('meta');
+            switch (name){
+                case 'description':
+                    meta = $('meta[name="description"]')
+                    break;
+                default:
+                    meta = $('meta[property="' + name+'"]')
+                    if (meta.length==0){
+                        meta = $('<meta property="'+name+'"></meta>').appendTo('head');
+                    }
+                    break;
+            }
+            meta.attr('content',value);
+        };
+
+        var setTitle = function(title){
+            $('title').text(title)
+        }
+
+        var setMetadata = function(metadata){
+            if (typeof metadata==='object'){
+                for (var key in metadata){
+                    switch(key){
+                        case 'title':
+                            setTitle(metadata[key]);
+                            break;
+                        default:
+                            setMetatag(key, metadata[key]);
+                            break;
+                    }
+                }
+            }
+        }
+
+        return {
+            setMetatag:setMetatag,
+            setTitle: setTitle,
+            setMetadata: setMetadata
+        }
+
     })
-    .run(function ($rootScope){
+    .service('socialShare', function (){
+        var tweet = {}
+        var shareLink = '';
+
+        return {
+            tweet: tweet,
+            setTweet: function(tweet){
+                this.tweet = tweet;
+            },
+
+            shareLink: shareLink
+        };
+
+    })
+    .run(function ($rootScope, socialShare){
+
+
         $rootScope.fbShare = function(href) {
 
             if (typeof href === 'undefined') {
@@ -48,4 +106,20 @@ angular.module('SEO-SMO', [])
                 href: href
             }, function() {});
         };
+
+        $rootScope.tweet = function (tweet){
+            var text= "";
+            if (typeof tweet ==='undefined'){
+                tweet= socialShare.tweet
+            }
+            if (typeof tweet === 'object'){
+                for(var key in tweet){
+                    text = text + "&"+key+"="+tweet[key];
+                }
+            }else if (typeof tweet === 'string'){
+                text = "text="+tweet
+            }
+            window.open("https://twitter.com/share?"+ text, "_blank");
+
+        }
     });
